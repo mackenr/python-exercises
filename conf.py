3d034272850c6d3cbe57d7e1e2fcd8afbcd736a4
Richard Macken <richard.c.macken@gmail.com>
2023-02-16 01:47:38 -0500

    ‘ok’

---
+++ conf.py
@@ -0,0 +1,15 @@
+extensions = ['sphinx_math_dollar', 'sphinx.ext.mathjax']
+
+mathjax_config = {
+    'tex2jax': {
+        'inlineMath': [ ["\\(","\\)"] ],
+        'displayMath': [["\\[","\\]"] ],
+    },
+}
+
+mathjax3_config = {
+  "tex": {
+    "inlineMath": [['\\(', '\\)']],
+    "displayMath": [["\\[", "\\]"]],
+  }
+}
